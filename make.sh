9e4e2e5290cabcc9b91ef5fb8c3ef188efcac31f
theia <theia@mit.edu>
2022-09-06 15:32:44 -0400

    compiling all files to www folder

--- make.sh
+++ make.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-pandoc \
-  --mathjax \
-  --listings \
-  -N \
-  -t html5 \
-  --variable linestretch=1.25 \
-  --output=notes/n00-intro.html \
-  notes/n00-intro.tex
+echo "Converting TeX to HTML..."
+for file in $(find . -name "*.tex")
+do
+  mkdir -p www/$(dirname $file)
+  pandoc \
+    --mathjax \
+    --listings \
+    -N \
+    -t html5 \
+    --variable linestretch=1.25 \
+    --output=www/${file%.tex}.html \
+    $file
+done
+
+echo "Injecting NB into HTML..."
+for file in $(find www -name "*.html")
+do
+  sed -i "/<\/head>/i <script src='https:\/\/nb.mit.edu\/embed_NB.js' type='text\/javascript'> <\/script>" $file
+done
+
+echo "Done."
