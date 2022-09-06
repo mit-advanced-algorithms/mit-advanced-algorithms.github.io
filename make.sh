52c99f22c637c6b9f15434ef1b8b604739be4a5a
theia <theia@mit.edu>
2022-09-06 15:09:45 -0400

    fixed line endings

--- make.sh
+++ make.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-echo "Hello World"
-pandoc 
-  --mathjax
-  --listings
-  -N
-  -t html5
-  --variable linestretch=1.25
-  --output=notes/n00-intro.html
+pandoc \
+  --mathjax \
+  --listings \
+  -N \
+  -t html5 \
+  --variable linestretch=1.25 \
+  --output=notes/n00-intro.html \
   notes/n00-intro.tex
