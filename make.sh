2a27f7dabc4fe817c7ce3ecf5849ec29d62b29e9
theia <theia@mit.edu>
2022-09-06 16:25:36 -0400

    move html files

--- make.sh
+++ make.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+rm -rf www
+
+echo "Copying over other HTML files..."
+for file in $(find . -name "*.html")
+do
+  mkdir -p www/$(dirname $file)
+  cp $file www/$file
+done
+
 echo "Converting TeX to HTML..."
 for file in $(find . -name "*.tex" -o -name "*.md")
 do
@@ -20,5 +29,3 @@ for file in $(find www -name "*.html")
 do
   sed -i "/<\/head>/i <script src='https:\/\/nb.mit.edu\/embed_NB.js' type='text\/javascript'> <\/script>" $file
 done
-
-echo "Done."
